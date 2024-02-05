4a6c61ff902f56ba6ec84356414abf4dd47eb382
Benoit Botton <benoit.botton@zerdalab.com>
2024-02-05 14:57:17 +0000

    [vite] allow folders/files FAIL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,21 @@
-import {defineConfig} from 'vite'
+import {defineConfig, searchForWorkspaceRoot } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   server: {
     fs: {
       strict: false,
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        // your custom rules
+        '../**/*',
+        '../resources',
+        '../resources/*.md',
+        'resources/**/*',        
+      ],
     },
   },
+  plugins: [vue()],
 })
---
+++ resources/test.md
@@ -0,0 +1,1 @@
+see if the assethandler can serve me
