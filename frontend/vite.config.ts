814913b96d15cd28c29bd6e36fa4be0f721b0cdd
Benoit Botton <benoit.botton@zerdalab.com>
2024-02-07 09:57:50 +0000

    partial fix. app not showing in dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,8 @@
-import {defineConfig, searchForWorkspaceRoot } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    fs: {
-      strict: false,
-      allow: [
-        // search up for workspace root
-        searchForWorkspaceRoot(process.cwd()),
-        // your custom rules
-        '../**/*',
-        '../resources',
-        '../resources/*.md',
-        'resources/**/*',        
-      ],
-    },
-  },
   plugins: [vue()],
+  appType: 'custom',
 })
